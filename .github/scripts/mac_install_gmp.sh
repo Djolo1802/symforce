173e367ab3597829c2364419f8a7032b2a3369e8
Djolo1802 <veljkovic.djordje@gmail.com>
2023-08-12 18:27:01 +0200

    more verbose more cache

--- .github/scripts/mac_install_gmp.sh
+++ .github/scripts/mac_install_gmp.sh
@@ -11,8 +11,8 @@ if [ "$CIBW_ARCHS_MACOS" = "arm64" ]; then
   mkdir arm-homebrew
   curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C arm-homebrew
   cache_path=$(./arm-homebrew/bin/brew --cache --build-from-source gmp)
-  echo $cache_path
-  curl --output -v $cache_path https://src.fedoraproject.org/repo/pkgs/gmp/gmp-6.2.1.tar.xz/sha512/c99be0950a1d05a0297d65641dd35b75b74466f7bf03c9e8a99895a3b2f9a0856cd17887738fa51cf7499781b65c049769271cbcb77d057d2e9f1ec52e07dd84/gmp-6.2.1.tar.xz
+  echo "Cache path: " $cache_path "test"
+  curl --output $cache_path -v https://src.fedoraproject.org/repo/pkgs/gmp/gmp-6.2.1.tar.xz/sha512/c99be0950a1d05a0297d65641dd35b75b74466f7bf03c9e8a99895a3b2f9a0856cd17887738fa51cf7499781b65c049769271cbcb77d057d2e9f1ec52e07dd84/gmp-6.2.1.tar.xz
   # parsed=($response)
   ./arm-homebrew/bin/brew install --build-from-source gmp
 fi
